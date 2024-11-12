8e32f808e3c455ab32c52d604857656621c85ce5
Martin <martinizquierdocuevas@gmail.com>
2024-11-12 16:37:38 +0100

    ADIOS

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HOLA");
-        System.out.println("ADIOS");
+        System.out.println("BYBYE");
 
     }
 }
